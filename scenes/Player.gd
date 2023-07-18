8a7e744d041a61c1924c276c1e745c5001bf7587
fremydev <fremy.dev@gmail.com>
2023-07-18 20:16:02 -0300

    improve player controller

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
-const GRAVITY: int = 300
-const MAXHSPEED: int = 100
-const JUMPSPEED: int = 100
+const GRAVITY: int = 1000
+const MAXHSPEED: int = 133
+const JUMPSPEED: int = 360
+const HORIZONTALAC: int = 2200
+const JUMPTERMULT: int = 4
 var velocity: Vector2 = Vector2.ZERO
 
 
@@ -11,9 +13,18 @@ func _process(delta):
 	move_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	move_vector.y = -1 if Input.is_action_just_pressed("jump") else 0
 	
-	velocity.x = move_vector.x * MAXHSPEED
+	velocity.x += move_vector.x * HORIZONTALAC * delta
+	if move_vector.x == 0:
+		velocity.x = lerp(0, velocity.x, pow(2, -55 * delta))
+		
+	velocity.x = clamp(velocity.x, -MAXHSPEED, MAXHSPEED)
 	
 	if move_vector.y < 0 and is_on_floor():
 		velocity.y = move_vector.y * JUMPSPEED
-	velocity.y += GRAVITY * delta
+		
+	if velocity.y < 0 and !Input.is_action_pressed("jump"):
+		velocity.y += GRAVITY * JUMPTERMULT * delta
+	else:
+		velocity.y += GRAVITY * delta
+		
 	velocity = move_and_slide(velocity, Vector2.UP)
